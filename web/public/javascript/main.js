be0e076ac90c45aaf7e88ac7ab097b34ecb60a72
gaseema.n <gaseema@bitsoko.io>
2018-11-19 04:21:46 +0300

    business profile update

--- web/public/javascript/main.js
+++ web/public/javascript/main.js
@@ -1,137 +1,173 @@
 (function($) {
-  
-  "use strict";  
 
-  $(window).on('load', function() {
+    "use strict";
+
+    $(window).on('load', function() {
 
 
-  /*Page Loader active
-  ========================================================*/
-  $('#preloader').fadeOut();
+        /*Page Loader active
+        ========================================================*/
+        $('#preloader').fadeOut();
 
-  // Sticky Nav
-    $(window).on('scroll', function() {
-        if ($(window).scrollTop() > 50) {
-            $('.scrolling-navbar').addClass('top-nav-collapse');
-        } else {
-            $('.scrolling-navbar').removeClass('top-nav-collapse');
-        }
-    });
-
-    // one page navigation 
-    $('.navbar-nav').onePageNav({
-      currentClass: 'active'
-    });
-
-    /* slicknav mobile menu active  */
-    $('.mobile-menu').slicknav({
-        prependTo: '.navbar-header',
-        parentTag: 'liner',
-        allowParentLinks: true,
-        duplicate: true,
-        label: '',
-        closedSymbol: '<i class="lni-chevron-right"></i>',
-        openedSymbol: '<i class="lni-chevron-down"></i>',
-      });
-
-      /* WOW Scroll Spy
-    ========================================================*/
-     var wow = new WOW({
-      //disabled for mobile
-        mobile: false
-    });
-
-    wow.init();
-
-     /* Testimonials Carousel 
-    ========================================================*/
-    var owl = $("#testimonials");
-      owl.owlCarousel({
-        loop: true,
-        nav: false,
-        dots: false,
-        center: true,
-        margin: 15,
-        slideSpeed: 1000,
-        stopOnHover: true,
-        autoPlay: true,
-        responsiveClass: true,
-        responsiveRefreshRate: true,
-        responsive : {
-            0 : {
-                items: 1
-            },
-            768 : {
-                items: 1
-            },
-            960 : {
-                items: 1
-            },
-            1200 : {
-                items: 1
-            },
-            1920 : {
-                items: 1
+        // Sticky Nav
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() > 50) {
+                $('.scrolling-navbar').addClass('top-nav-collapse');
+            } else {
+                $('.scrolling-navbar').removeClass('top-nav-collapse');
             }
-        }
-      });  
+        });
 
-     /*  Slick Slider
+        // one page navigation
+        $('.navbar-nav').onePageNav({
+            currentClass: 'active'
+        });
+
+        /* slicknav mobile menu active  */
+        $('.mobile-menu').slicknav({
+            prependTo: '.navbar-header',
+            parentTag: 'liner',
+            allowParentLinks: true,
+            duplicate: true,
+            label: '',
+            closedSymbol: '<i class="lni-chevron-right"></i>',
+            openedSymbol: '<i class="lni-chevron-down"></i>',
+        });
+
+        /* WOW Scroll Spy
     ========================================================*/
-    $('.slider-center').slick({
-      centerMode: true,
-      centerPadding: '60px',
-      slidesToShow: 3,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            arrows: false,
+        var wow = new WOW({
+            //disabled for mobile
+            mobile: false
+        });
+
+        wow.init();
+
+        /* Testimonials Carousel
+    ========================================================*/
+        var owl = $("#testimonials");
+        owl.owlCarousel({
+            loop: true,
+            nav: false,
+            dots: false,
+            center: true,
+            margin: 15,
+            slideSpeed: 1000,
+            stopOnHover: true,
+            autoPlay: true,
+            responsiveClass: true,
+            responsiveRefreshRate: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 1
+                },
+                960: {
+                    items: 1
+                },
+                1200: {
+                    items: 1
+                },
+                1920: {
+                    items: 1
+                }
+            }
+        });
+
+        /*  Slick Slider
+    ========================================================*/
+        $('.slider-center').slick({
             centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 1
-          }
-        }
-      ]
+            centerPadding: '60px',
+            slidesToShow: 3,
+            responsive: [{
+                    breakpoint: 768,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        });
+
+
+        /* Back Top Link active
+        ========================================================*/
+        var offset = 200;
+        var duration = 500;
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > offset) {
+                $('.back-to-top').fadeIn(400);
+            } else {
+                $('.back-to-top').fadeOut(400);
+            }
+        });
+
+        $('.back-to-top').on('click', function(event) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: 0
+            }, 600);
+            return false;
+        });
+
+        /* Map Form Toggle
+        ========================================================*/
+        $('.map-icon').on('click', function(e) {
+            $('#conatiner-map').toggleClass('panel-show');
+            e.preventDefault();
+        });
+
     });
-    
-
-    /* Back Top Link active
-    ========================================================*/
-      var offset = 200;
-      var duration = 500;
-      $(window).scroll(function() {
-        if ($(this).scrollTop() > offset) {
-          $('.back-to-top').fadeIn(400);
-        } else {
-          $('.back-to-top').fadeOut(400);
-        }
-      });
-
-      $('.back-to-top').on('click',function(event) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: 0
-        }, 600);
-        return false;
-      });
-
-      /* Map Form Toggle
-      ========================================================*/
-      $('.map-icon').on('click',function (e) {
-          $('#conatiner-map').toggleClass('panel-show');
-          e.preventDefault();
-      });
-
-  });      
 
 }(jQuery));
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive: {
+        // breakpoint from 0 up
+        0: {
+            items: 1,
+        },
+        // breakpoint from 480 up
+        480: {
+            items: 1,
+        },
+        560: {
+            items: 2,
+        },
+        // breakpoint from 768 up
+        880: {
+            items: 3,
+        },
+        // breakpoint from 768 up
+        1380: {
+            items: 4,
+        }
+    }
+});
+// Go to the next item
+$('.customNextBtn').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.customPrevBtn').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
+})
